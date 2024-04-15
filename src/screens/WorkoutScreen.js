cd24e3ef7b7c01a8371379ce4e0522a601ebc405
Frederichdk <fw21deko@siena.edu>
2024-04-15 16:26:02 -0400

    Initial set up for workout complete

--- src/screens/WorkoutScreen.js
+++ src/screens/WorkoutScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
 
 import { ScreenContainer } from "../components/ScreenContainer";
 
@@ -9,34 +9,74 @@ import { Container } from "../components/Container";
 import { CustomButton } from "../components/CustomButton";
 import { useState } from "react";
 import { InputField } from "../components/InputField";
+import { useEffect } from "react";
 
 export const WorkoutScreen = () => {
   const [workoutName, setWorkoutName] = useState("");
+  const [startTime, setTime] = React.useState("");
+  const [endTime, setEndTime] = React.useState("");
 
   const handleWorkoutNameChange = (text) => {
     setWorkoutName(text);
   };
 
+  React.useEffect(() => {
+    setTime(new Date().toLocaleTimeString());
+  }, []);
+
+  const getTime = () => {
+    setEndTime(new Date().toLocaleTimeString());
+  };
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (text) => {
+    // Allow only numbers
+    const numericValue = text.replace(/[^0-9]/g, "").slice(0, 3);
+    setInputValue(numericValue);
+  };
+
   return (
     <ScreenContainer style={styles.infoPlace}>
       <Container style={styles.infoContainer}>
-        <Container style={styles.textbox}>
+        <Container style={[styles.nameBox]}>
           <InputField
             label={"Workout Name"}
             keyboardType="default"
             value={workoutName}
             onChangeText={handleWorkoutNameChange}
-            style={{ marginBottom: 0 }}
+            style={{
+              marginBottom: 0,
+              paddingBottom: 0,
+              borderBottomWidth: 0,
+            }}
           />
         </Container>
-        <Container style={styles.textbox}>
+        <Container
+          style={[styles.textbox, { justifyContent: "space-between" }]}
+        >
           <Text style={styles.text}>Start time:</Text>
+          <Text style={styles.text}>{startTime}</Text>
         </Container>
-        <Container style={styles.textbox}>
+        <Container
+          style={[styles.textbox, { justifyContent: "space-between" }]}
+        >
           <Text style={styles.text}>End time:</Text>
+          <Text style={styles.text}>{endTime}</Text>
         </Container>
-        <Container style={styles.textbox}>
+        <Container
+          style={[styles.textbox, { justifyContent: "space-between" }]}
+        >
           <Text style={styles.text}>BodyWeight:</Text>
+          <TextInput
+            style={styles.text}
+            onChangeText={handleChange}
+            value={inputValue}
+            keyboardType="number-pad"
+            returnKeyType={"done"}
+            placeholder="numbers only"
+            placeholderTextColor="#999"
+          />
         </Container>
       </Container>
       <CustomButton
@@ -46,6 +86,9 @@ export const WorkoutScreen = () => {
       <CustomButton
         style={styles.finishWorkout}
         label={"Finish Workout"}
+        onPress={() => {
+          getTime();
+        }}
       ></CustomButton>
     </ScreenContainer>
   );
@@ -56,10 +99,21 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "center",
     height: "fit-content",
+    marginBottom: 20,
   },
 
   infoPlace: {
     justifyContent: "flex-start",
+    alignItems: "center",
+  },
+
+  nameBox: {
+    width: "98%",
+    height: "fit-content",
+    marginTop: 0,
+    marginBottom: 0,
+    paddingBottom: 0,
+    justifyContent: "center",
     alignItems: "center",
   },
 
@@ -70,8 +124,8 @@ const styles = StyleSheet.create({
     boarderColor: colors.black,
     width: "98%",
     height: 50,
-    marginTop: 5,
-    marginBottom: 5,
+    marginTop: 4,
+    marginBottom: 0,
     justifyContent: "left",
     alignItems: "center",
     padding: 0,
@@ -79,6 +133,7 @@ const styles = StyleSheet.create({
 
   text: {
     marginLeft: 10,
+    marginRight: 10,
   },
 
   exersiceContainer: {
