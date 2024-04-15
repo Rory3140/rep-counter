714b73fc0824d479439c4f5c1ca7d1336a5cb380
Frederichdk <fw21deko@siena.edu>
2024-04-15 13:22:45 -0400

    rough date time picker added

--- src/screens/WorkoutScreen.js
+++ src/screens/WorkoutScreen.js
@@ -7,12 +7,43 @@ import { colors } from "../utils/colors";
 import { sizes, fontSizes } from "../utils/spacing";
 import { Container } from "../components/Container";
 import { CustomButton } from "../components/CustomButton";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useState } from "react";
 
 export const WorkoutScreen = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (e, selectedDate) => {
+    setDate(selectedDate);
+  };
+
   return (
     <ScreenContainer style={styles.infoPlace}>
       <Container style={styles.infoContainer}>
-        <Text>workout info</Text>
+        <Container style={styles.textbox}>
+          <Text style={styles.text}>workout info</Text>
+        </Container>
+        <Container style={styles.textbox}>
+          <Text style={styles.text}>Start time</Text>
+          <DateTimePicker
+            value={date}
+            mode={"date"}
+            is24Hour={true}
+            onChange={onChange}
+          />
+          <DateTimePicker
+            value={date}
+            mode={"time"}
+            is24Hour={true}
+            onChange={onChange}
+          />
+        </Container>
+        <Container style={styles.textbox}>
+          <Text style={styles.text}>End time</Text>
+        </Container>
+        <Container style={styles.textbox}>
+          <Text style={styles.text}>BodyWeight</Text>
+        </Container>
       </Container>
       <CustomButton
         style={styles.exersiceContainer}
@@ -27,11 +58,32 @@ export const WorkoutScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  infoContainer: {},
+  infoContainer: {
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: "fit-content",
+  },
 
   infoPlace: {
     justifyContent: "flex-start",
     alignItems: "center",
+  },
+
+  textbox: {
+    display: "flex",
+    flexDirection: "row",
+    boarderwidth: 1,
+    boarderColor: colors.black,
+    width: "98%",
+    height: 35,
+    marginTop: 5,
+    marginBottom: 5,
+    justifyContent: "center",
+    alignItems: "left",
+  },
+
+  text: {
+    marginLeft: 10,
   },
 
   exersiceContainer: {
