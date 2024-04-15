9eb1336b8c69910f34a0f77653e4248bd14932e5
Frederichdk <fw21deko@siena.edu>
2024-04-15 12:30:47 -0400

    Workout changes

--- src/screens/WorkoutScreen.js
+++ src/screens/WorkoutScreen.js
@@ -5,11 +5,39 @@ import { ScreenContainer } from "../components/ScreenContainer";
 
 import { colors } from "../utils/colors";
 import { sizes, fontSizes } from "../utils/spacing";
+import { Container } from "../components/Container";
+import { CustomButton } from "../components/CustomButton";
 
 export const WorkoutScreen = () => {
   return (
-    <ScreenContainer>
-      <Text>Workout</Text>
+    <ScreenContainer style={styles.infoPlace}>
+      <Container style={styles.infoContainer}>
+        <Text>workout info</Text>
+      </Container>
+      <CustomButton
+        style={styles.exersiceContainer}
+        label={"Add Exersice"}
+      ></CustomButton>
+      <CustomButton
+        style={styles.finishWorkout}
+        label={"Finish Workout"}
+      ></CustomButton>
     </ScreenContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  infoContainer: {},
+
+  infoPlace: {
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+
+  exersiceContainer: {
+    height: 50,
+  },
+  finishWorkout: {
+    height: 50,
+  },
+});
