651a6b65ec63201725c0aeb246121c5c64bb6a72
Frederichdk <fw21deko@siena.edu>
2024-04-16 16:11:59 -0400

    Started on routines, and to make change in custom button, left comments explaining. if it messes anything up feel free to change it back

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -3,13 +3,12 @@ import { Text, TouchableOpacity } from "react-native";
 
 import { colors } from "../utils/colors";
 import { sizes, fontSizes } from "../utils/spacing";
-
-export const CustomButton = ({ label, onPress, style }) => {
+//I added a textColor prop, this was to change the color of the text when I changed the background color of the button
+export const CustomButton = ({ label, onPress, style, textColor }) => {
   return (
     <TouchableOpacity
       onPress={onPress}
       style={[
-        style,
         {
           backgroundColor: colors.primary,
           padding: 20,
@@ -17,15 +16,21 @@ export const CustomButton = ({ label, onPress, style }) => {
           margin: 10,
           width: "95%",
         },
+        style,
+        //This stylle was initially above this curly brace, I move it after because the
+        //color i was changing using the style prop was beingh overriden by the inline after it
       ]}
     >
       <Text
-        style={{
-          textAlign: "center",
-          fontWeight: "700",
-          fontSize: 16,
-          color: "#fff",
-        }}
+        style={[
+          {
+            textAlign: "center",
+            fontWeight: "700",
+            fontSize: 16,
+            color: textColor || "#fff",
+            //by default it will stay white, with the prop you can acces the color
+          },
+        ]}
       >
         {label}
       </Text>
--- src/screens/RoutinesScreen.js
+++ src/screens/RoutinesScreen.js
@@ -1,15 +1,49 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-
+import { CustomButton } from "../components/CustomButton";
 import { ScreenContainer } from "../components/ScreenContainer";
-
 import { colors } from "../utils/colors";
 import { sizes, fontSizes } from "../utils/spacing";
+import { Container } from "../components/Container";
 
 export const RoutinesScreen = () => {
   return (
     <ScreenContainer>
-      <Text>Routines</Text>
+      <CustomButton label={"Create New Routine"} onPress={() => {}} />
+      <Container style={styles.Line}></Container>
+      <CustomButton
+        label={"Routine 1(White backround)Arrow to right"}
+        style={styles.routineButton}
+        onPress={() => {}}
+        textColor={colors.black}
+      />
     </ScreenContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  Line: {
+    backgroundColor: colors.offWhite,
+    borderRadius: 0,
+    borderTopColor: colors.darkGrey,
+    borderTopWidth: 3,
+    borderWidth: 0,
+    width: "95%",
+    padding: 0,
+    margin: 0,
+  },
+
+  routineButton: {
+    backgroundColor: colors.white,
+    width: "95%",
+    height: "fit-content",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: sizes.md,
+    borderWidth: 1,
+    borderColor: colors.lightGrey,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 20,
+  },
+});
