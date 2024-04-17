64bd8de92301891d8990e72fc1e8ff2f95a04e68
frederichdk <fw21deko@siena.edu>
2024-04-17 15:58:37 -0400

    Routine updated, creation almost working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
-
+import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { colors } from "../utils/colors";
 import { sizes, fontSizes } from "../utils/sizes";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
-export const Button = ({ label, onPress, style, textColor }) => {
+export const Button = ({
+  label,
+  onPress,
+  style,
+  textColor,
+  isVis = false,
+  flexDirection,
+}) => {
   return (
     <TouchableOpacity
       onPress={onPress}
@@ -19,18 +27,34 @@ export const Button = ({ label, onPress, style, textColor }) => {
         style,
       ]}
     >
-      <Text
-        style={[
-          {
-            textAlign: "center",
-            fontWeight: "bold",
-            fontSize: fontSizes.md,
-            color: textColor || colors.white,
-          },
-        ]}
+      <View
+        style={[styles.arrow, { flexDirection: flexDirection || "column" }]}
       >
-        {label}
-      </Text>
+        <Text
+          style={[
+            {
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: fontSizes.md,
+              color: textColor || colors.white,
+            },
+          ]}
+        >
+          {label}
+        </Text>
+
+        {isVis && (
+          <MaterialIcons name="arrow-forward-ios" size={22} color="black" />
+        )}
+      </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  arrow: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "98%",
+  },
+});
